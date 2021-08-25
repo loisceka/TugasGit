c9301f4549d4714f450aadd0d7562ad407f7f9d4
kevithamonding <kevithapraisy@gmail.com>
2021-08-25 12:02:05 +0800

    add sout Tugas GIthub & OOP

--- TugasOop/src/tugasoop/TugasOop.java
+++ TugasOop/src/tugasoop/TugasOop.java
@@ -15,7 +15,7 @@ public class TugasOop {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Tugas Github & OOP");
     }
     
 }
